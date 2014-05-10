38121baa84add8f6fa136b8ee129a4265002ece3
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-11 03:03:32 +0900

    implement manage method

--- index.js
+++ index.js
@@ -28,3 +28,40 @@ Zmanager.prototype.create = function () {
 
   return zmanagerc;
 };
+
+Zmanager.prototype.manage = function (zmanagerc) {
+  var ast = parse(this.css);
+  var z_selectors = [];
+  var z_num = getPropNum(zmanagerc);
+
+  for (var i = 1; i <= z_num; i++) {
+    var key = '' + i;
+    z_selectors.push(zmanagerc.key);
+  }
+
+  z_selectors.forEach(function (z_selector) {
+    ast.stylesheet.rules.forEach(function visit (rule) {
+      if (rule.rules) rule.rules.forEach(visit);
+
+      if (rule.selector === z_selector) {
+        rule.declarations.forEach(function (declaration) {
+          if (declaration.property === 'z-index') {
+            if (declaration.value === 'auto') return;
+
+            declaration.value = z_num--;
+          }
+        });
+      }
+    });
+  });
+
+  return ast;
+};
+
+function getPropNum (obj) {
+  var len = 0;
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) ++len;
+  }
+  return len;
+};
